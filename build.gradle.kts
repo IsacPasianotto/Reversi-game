ad5a779c430623d9893d0d68a9e8ae12a54c48b4
pasianeight <yh4mop12z@gmail.com>
2024-01-15 15:00:43 +0100

    removed warning about deprecations in gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,8 +10,8 @@ repositories {
 }
 
 dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.9.1"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
+    testImplementation("org.junit.jupiter:junit-jupiter:5.9.2")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
     testImplementation("org.mockito:mockito-core:3.12.4")
     testImplementation("org.mockito:mockito-junit-jupiter:3.12.4")
 
